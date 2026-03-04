ea1045eeccbaefe06cad8aa5be38de1d02ab0775
neil <neil.manzano@outlook.fr>
2026-03-04 12:19:23 +0100

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 #   end
 
 User.destroy_all
+List.destroy_all
+Movie.destroy_all
+Link.destroy_all
 
 User.find_or_create_by!(email: "test@test.com") do |u|
   u.first_name = "Jean"
@@ -17,3 +20,51 @@ User.find_or_create_by!(email: "test@test.com") do |u|
 end
 
 puts "Seed OK user type : test@test.com / password123"
+
+Movie.new(
+  overview: "tres bon film",
+  poster_path:"url",
+  rate_average: 3.2,
+  title: "mon film",
+  tmdb_id: 1
+)
+Movie.new(
+  overview: "tres tres bon film",
+  poster_path: "url",
+  rate_average: 4.2,
+  title: "the film",
+  tmdb_id: 2
+)
+
+puts "Seed OK list create"
+
+List.new(
+  name: "Ma list",
+  prompt: "film comique",
+  user_id: 1
+)
+
+List.new(
+  name: "T'as list",
+  prompt: "film comique",
+  user_id: 1
+)
+
+  puts "Seed OK list create"
+
+Link.new(
+  list_id: 1,
+  movie_id: 2
+)
+
+Link.new(
+  list_id: 1,
+  movie_id: 1
+)
+
+Link.new(
+  list_id: 2,
+  movie_id: 2
+)
+
+puts "Seed OK link create"
