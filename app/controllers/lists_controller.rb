e8164e22f3915a4a1201f4dcb651878a6e152beb
Mathis <kmzmathis@gmail.com>
2026-03-04 13:14:20 +0100

    Creation de list OK sans LLM

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,22 @@ class ListsController < ApplicationController
     def index
     # @lists = current_user.lists
     end
+    def new
+      @list = List.new
+    end
+
+    def create
+      list = List.new(list_params)
+      list.user_id = current_user.id
+      if list.save
+        redirect_to list_path(list)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+    def list_params
+      params.require(:list).permit(:name, :prompt, :user_id)
+    end
 end
