f3ddf48244425fadcc89edb7f253215bf1989693
neil <neil.manzano@outlook.fr>
2026-03-06 10:11:20 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "lists#index"
+  root "pages#home"
 
   resources :movies, only: [:show]
 
